7211addc213acf69e226fe96cb249cda486b45b2
Jochen Schirrwagen <jochen.schirrwagen@uni-bielefeld.de>
2018-06-25 16:10:04 +0200

    updated year

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'OpenAIRE Guidelines for Literature Repository Managers'
-copyright = u'2017, OpenAIRE. This work is licensed under Creative Commons Attribution 4.0 International'
+copyright = u'2018, OpenAIRE. This work is licensed under Creative Commons Attribution 4.0 International'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
