c0fb29b368c83ac3fd07d49b46f6cd852bd0ebf7
Jochen Schirrwagen <jochen.schirrwagen@uni-bielefeld.de>
2018-11-29 17:12:42 +0100

    use papersize a4

--- docs/conf.py
+++ docs/conf.py
@@ -216,7 +216,7 @@ latex_elements = {
           %\makeatother
            ''',
 # The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
 #'pointsize': '10pt',
