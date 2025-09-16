42b46542003c32331959ac41ed95533e5d11da4b
Leonidas Pispiringas <lpispir@gmail.com>
2025-09-16 13:22:23 +0300

    Update conf.py
    unicode characters fix

--- docs/conf.py
+++ docs/conf.py
@@ -198,7 +198,7 @@ htmlhelp_basename = 'OpenAIREGuidelinesdoc'
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
-        'preamble': '''
+        'preamble': r'''
            \usepackage[none]{hyphenat}
           % \usepackage{fancyhdr}
           % \pagestyle{fancy}
