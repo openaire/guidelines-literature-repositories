fe151da22dd4d98a3c5b46ed3095a5094ceb5df2
Leonidas Pispiringas <lpispir@gmail.com>
2025-11-28 10:34:05 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -301,6 +301,6 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 # OLD Sphinx: intersphinx_mapping = {'http://docs.python.org/': None}
 intersphinx_mapping = {
-    "python": ("https://docs.python.org/3", None),
+    "python": ("https://docs.python.org/3", None)
 }
 
